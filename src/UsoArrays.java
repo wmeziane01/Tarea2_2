1bcafd92a0db1430405c52dc3b5e012caffd932d
wmeziane01 <wmeziane01@educarex.es>
2023-12-05 10:13:47 +0100

    Inicio proyeto

---
+++ src/UsoArrays.java
@@ -0,0 +1,9 @@
+public class UsoArrays {
+public static void main(String[] args) {
+ // TODO Esbozo de método generado automáticamente
+
+ Arrays miArray=new Arrays(5);
+
+ miArray.getDatosArrays();
+ }
+}
